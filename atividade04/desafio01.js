2b97313e10ea1953461565e1fdc471670357a5a4
Vithor Félix <91703672+vithorfjm@users.noreply.github.com>
2023-09-13 16:08:22 -0300

    Desafio 01 da atividade 4

---
+++ atividade04/desafio01.js
@@ -0,0 +1,31 @@
+const numeros = prompt("Digite 5 números separados por virgula. Exemplo: 10,20,30,40,50");
+
+function printarNumeros(numerosStr) {
+    numerosArray = numerosStr.split(",");
+
+    // Verifica se a quantidade de números inserida está correta
+    if (numerosArray.length != 5) {
+        console.log("ERRO: quantidade de números inválida");
+        return;
+    }
+    
+    // Verifica se os itens passados são do tipo 'number'
+    const caracteresInvalidos = [];
+    numerosArray.forEach((num) => {
+        if (isNaN(num)) {
+            caracteresInvalidos.push(num);
+        }
+    })
+
+    // Informa quais são os caracteres inválidos, caso tenha algum
+    if (caracteresInvalidos.length != 0) {
+        console.log(`ERRO: "${caracteresInvalidos}" não são caracteres válidos`);
+        return;
+    }
+
+    // Printa os números digitados
+    console.log(`Números digitados: ${numerosArray}`);
+
+}
+
+printarNumeros(numeros);
